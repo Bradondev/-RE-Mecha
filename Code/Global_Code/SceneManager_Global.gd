e6d8fbb969e533422f86ee5b7733928f18d8d2a3
Brandon Martin Del Campo <brandondevprog@gmail.com>
2025-02-03 20:06:43 -0800

    Duel tile set Up

--- Code/Global_Code/SceneManager_Global.gd
+++ Code/Global_Code/SceneManager_Global.gd
@@ -3,4 +3,13 @@ extends Node
 
 
 func  ChangeScene(NewScene:String)->void:
+	get_tree().change_scene_to_file(NewScene)
+	await Transition()
 	pass
+
+
+func  Transition(TransitonOut:bool = true)->void:
+	if TransitonOut:
+		pass
+	else:
+		pass
